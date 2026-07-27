2cce5a8911f8d2a720930ed45b96231c23083167
YKbabu77 <yepugantikondababu@gmail.com>
2026-07-27 13:36:14 +0530

    Added CategoryController with CRUD APIs

---
+++ restaurant-backend/src/main/java/com/restaurant/restaurant_backend/controller/CategoryController.java
@@ -0,0 +1,66 @@
+package com.restaurant.restaurant_backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restaurant.restaurant_backend.entity.Category;
+import com.restaurant.restaurant_backend.service.CategoryService;
+
+@RestController
+@RequestMapping("/api/categories")
+@CrossOrigin(origins = "*")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    // Create Category
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        return ResponseEntity.ok(categoryService.saveCategory(category));
+    }
+
+    // Get All Categories
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+    // Get Category By ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+
+        return categoryService.getCategoryById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // Update Category
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id,
+                                                   @RequestBody Category category) {
+
+        return ResponseEntity.ok(categoryService.updateCategory(id, category));
+    }
+
+    // Delete Category
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long id) {
+
+        categoryService.deleteCategory(id);
+
+        return ResponseEntity.ok("Category deleted successfully.");
+    }
+
+}
